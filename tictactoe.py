f583cfec98519a5d403d69f25abe498f52d02a47
bk-gill <132428715+bk-gill@users.noreply.github.com>
2023-06-02 15:56:35 -0600

    Implemented a VS Computer mode utiltizing the random module

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import random
+
 def print_board(board):
     for row in board:
         print("|", end=" ")
@@ -10,7 +12,7 @@ def check_winner(board):
     for row in board:
         if row[0] == row[1] == row[2] != " ":
             return row[0]
-        
+
     for col in range(3):
         if board[0][col] == board[1][col] == board[2][col] != " ":
             return board[0][col]
@@ -23,31 +25,64 @@ def check_winner(board):
     return None
 
 
+def get_player_move(board):
+    row = int(input("Enter the row (0-2): "))
+    col = int(input("Enter the column (0-2): "))
+
+    if board[row][col] != " ":
+        print("Invalid move! Try again.")
+        return get_player_move(board)
+
+    return row, col
+
+
+def get_computer_move(board):
+    available_moves = []
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == " ":
+                available_moves.append((row, col))
+
+    return random.choice(available_moves)
+
+
 def play_game():
     board = [[" " for _ in range(3)] for _ in range(3)]
     current_player = "X"
     winner = None
     moves_left = 9
 
+    game_mode = input("Select game mode (PvP/PvC): ")
+    if game_mode.lower() == "pvp":
+        is_player_vs_player = True
+    else:
+        is_player_vs_player = False
+
     while moves_left > 0 and not winner:
         print_board(board)
         print("It's", current_player, "turn.")
 
-        row = int(input("Enter the row (0-2): "))
-        col = int(input("Enter the column (0-2): "))
-
-        if board[row][col] == " ":
-            board[row][col] = current_player
-            winner = check_winner(board)
-            current_player = "O" if current_player == "X" else "X"
-            moves_left -= 1
+        if (is_player_vs_player and current_player == "X") or not is_player_vs_player:
+            row, col = get_player_move(board)
         else:
-            print("Invalid move! Try again.")
+            row, col = get_computer_move(board)
+            print("Computer selects row:", row, "and column:", col)
+
+        board[row][col] = current_player
+        winner = check_winner(board)
+        current_player = "O" if current_player == "X" else "X"
+        moves_left -= 1
 
     print_board(board)
 
     if winner:
-        print("Player", winner, "wins!")
+        if is_player_vs_player:
+            print("Player", winner, "wins!")
+        else:
+            if winner == "X":
+                print("You win!")
+            else:
+                print("Computer wins!")
     else:
         print("It's a tie!")
 
